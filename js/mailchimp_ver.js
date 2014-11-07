bac3fd076dd8ba5d339dc9b7718e66803172c348
Alan Plotko <alan.plotko@gmail.com>
2014-11-07 17:22:13 -0500

    Complete MailChimp form

--- js/mailchimp_ver.js
+++ js/mailchimp_ver.js
@@ -2,27 +2,62 @@
 	Signup Form - Verification Functions
 ------------------------------------------*/
 
+// Criteria to successfully submit
+var emailCheck, fNameCheck, lNameCheck = false;
+
 $("#mce-EMAIL").on("focusout", function() {
-	var result = isValidEmailAddress(this.value, $("#mce-EMAIL").val().length);
-    if(result) {
-    	$('#email-hint').text("Valid email address!");
-    	$("#mce-EMAIL ~ div").toggleClass("error-visible", false);
-    	$("#mce-EMAIL").toggleClass("unverifiedEmail", false);
-    	$("#mce-EMAIL").toggleClass("verifiedEmail", true);
+    var email = this.value;
+    emailCheck = isValidEmailAddress(email);
+
+    if(email.length > 0 && emailCheck) {
+        $('#email-fdbk').text("Valid email address!");              // Feedback
+        $("#mce-EMAIL ~ div").toggleClass("inputFeedback", false);  // Hide errors
+        $("#mce-EMAIL").toggleClass("unverifiedInput", false);      // Hide unverified (red) input styling
+        $("#mce-EMAIL").toggleClass("verifiedInput", true);         // Show verified (green) input styling
     }
     else {
-    	$('#email-hint').text("Error: Invalid email address");
-    	$("#mce-EMAIL").toggleClass("error-visible", true);
-    	$("#mce-EMAIL").toggleClass("verifiedEmail", false);
-    	$("#mce-EMAIL").toggleClass("unverifiedEmail", true);
+        $('#email-fdbk').text("Error: Invalid email address");      // Feedback
+        $("#mce-EMAIL").toggleClass("inputFeedback", true);         // Show errors
+        $("#mce-EMAIL").toggleClass("verifiedInput", false);        // Hide verified (green) input styling
+        $("#mce-EMAIL").toggleClass("unverifiedInput", true);       // Show unverified (red) input styling
+        emailCheck = false;
     }
+    checkCriteria();
 });
 
+$("#mce-FNAME").on("focusout", function() {
+    fNameCheck = isValidName(this);
+    checkCriteria();
+});
 
-function isValidEmailAddress(email, length) {
-	if (length > 0) {
-		var emailReg = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i);
-		return emailReg.test(email);
-	}
-	return false;
+$("#mce-LNAME").on("focusout", function() {
+    lNameCheck = isValidName(this);
+    checkCriteria();
+});
+
+function isValidEmailAddress(email) {
+	var emailReg = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i);
+	return emailReg.test(email);
+}
+
+function isValidName(e) {
+    if(e.value.length > 0) {
+        $(e).toggleClass("unverifiedInput", false); // Hide unverified (red) input styling
+        $(e).toggleClass("verifiedInput", true);    // Show verified (green) input styling
+        return true;
+    }
+    else {
+        $(e).toggleClass("verifiedInput", false);   // Hide verified (green) input styling
+        $(e).toggleClass("unverifiedInput", true);  // Show unverified (red) input styling
+        return false;
+    }
+}
+
+function checkCriteria() {
+    if(emailCheck && fNameCheck && lNameCheck) {
+        $("#mc-embedded-subscribe").prop("disabled", false);
+    }
+    else {
+        $("#mc-embedded-subscribe").prop("disabled", true);
+    }
 }
